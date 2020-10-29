3ce0a57cda9709021075f00bca5977b0ae88a451
邵燃 <chengfubeiming@gmail.com>
2020-10-29 14:39:22 +0800

    'modified_example'

--- example/hello_world.dart
+++ example/hello_world.dart
@@ -3,18 +3,16 @@ import 'package:hetu_script/hetu_script.dart';
 void main() async {
   var hetu = await Hetu.init(externalFunctions: {
     'dartHello': (HT_Instance instance, List<dynamic> args) {
-      print('hello from dart');
-      if (args.isNotEmpty) {
-        for (final arg in args) {
-          print(arg);
-        }
-      }
+      return {'dartValue': 'hello'};
     },
   });
   hetu.eval(
       'external fun dartHello\n'
       'proc main {\n'
-      'dartHello("from hetu")\n'
+      '  var dartValue = dartHello()\n'
+      '  print(typeof(dartValue))\n'
+      '  dartValue[\'foo\'] = \'bar\'\n'
+      '  print(dartValue)'
       '\n}',
       invokeFunc: 'main');
 }
