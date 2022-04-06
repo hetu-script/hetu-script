82c3925402d3f7a71dc16c5608f447e4e36b87e8
邵燃 <chengfubeiming@gmail.com>
2022-04-06 17:40:55 +0800

    fix typeof operator bug

--- test/test.dart
+++ test/test.dart
@@ -19,19 +19,7 @@ void main() {
   hetu.init(locale: HTLocaleSimplifiedChinese());
 
   final r = hetu.eval(r'''
-    struct B {
-      var age = 42
-      construct (age: num) {
-        this.age = age
-      }
-    }
-    final t = typeof B
-    print(t)
-    
-    type T = {
-      age: int
-    }
-    print(B is T)
+    print(typeof 1)
   ''');
 
   print(r);
