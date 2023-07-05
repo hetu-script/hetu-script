de02fb124c6bab2912c5eb7ebd87e0ff0d737ead
Ran Shao <chengfubeiming@live.com>
2023-07-05 19:49:51 +0800

    update codepad to fix the setter bug

--- packages/hetu_script_code_pad/ios/Flutter/flutter_export_environment.sh
+++ packages/hetu_script_code_pad/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=F:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=G:\_dev\hetu-script\packages\hetu_script_code_pad"
+export "FLUTTER_ROOT=D:\_dev\_SDKs\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\_dev\hetu-script\packages\hetu_script_code_pad"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
